c8285c1e4cbc835d45f6d0f096d718ae636ba2a5
여동준 <tgc03042@naver.com>
2022-09-26 15:39:30 +0900

    Sub3_12_finish

--- src/ssafy_3/scripts/camera_pedes_detector.py
+++ src/ssafy_3/scripts/camera_pedes_detector.py
@@ -23,19 +23,14 @@ from cv_bridge import CvBridgeError
 # 8. 추출된 좌표 Detection 용 박스 화
 
 def non_maximum_supression(bboxes, threshold=0.3):
-    
     bboxes = sorted(bboxes, key=lambda detections: detections[3],
             reverse=True)
     new_bboxes=[]
-    
     new_bboxes.append(bboxes[0])
-    
     bboxes.pop(0)
 
     for _, bbox in enumerate(bboxes):
-
         for new_bbox in new_bboxes:
-
             x1_tl = bbox[0]
             x2_tl = new_bbox[0]
             x1_br = bbox[0] + bbox[2]
@@ -46,22 +41,19 @@ def non_maximum_supression(bboxes, threshold=0.3):
             y2_br = new_bbox[1] + new_bbox[3]
 
             #TODO: (7)  IOU 계산
-            '''
-            이미지의 IoU를 계산하는 영역입니다.
-            위에 입력된 파라미터를 이용하여 IoU를 계산 해 보시기 바랍니다.
-            x_overlap =
-            y_overlap =
-            overlap_area =
+            # 이미지의 IoU를 계산하는 영역입니다.
+            # 위에 입력된 파라미터를 이용하여 IoU를 계산 해 보시기 바랍니다.
+            x_overlap = x2_tl - x1_br
+            y_overlap = y2_tl - y1_br
+            overlap_area = abs(x_overlap * y_overlap)
             
-            area_1 =
-            area_2 =
+            area_1 = abs((x1_tl - x1_br) * (y1_tl - y1_br))
+            area_2 = abs((x2_tl - x2_br) * (y2_tl - y2_br))
             
-            total_area =
-
-            overlap_area =
-            '''
+            total_area = area_1 + area_2 - overlap_area
+            
+            overlap_area = overlap_area / (total_area + 1e-5)
             if overlap_area < threshold:
-                
                 new_bboxes.append(bbox)
 
     return new_bboxes
@@ -70,13 +62,11 @@ def non_maximum_supression(bboxes, threshold=0.3):
 
 class PEDESDetector:
     def __init__(self):
-
         #TODO: (1) subscriber 선언
         self.image_sub = rospy.Subscriber(
             "/image_jpeg/compressed",
             CompressedImage,
             self.callback)
-
         self.rate = rospy.Rate(20)
 
         #TODO: (2) HOG descitpor 생성
@@ -86,7 +76,6 @@ class PEDESDetector:
         self.pedes_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
     def callback(self, msg):
-
         self.rate.sleep()
 
         try:
@@ -100,14 +89,12 @@ class PEDESDetector:
 
         #TODO: (5)  검출 (paramter 바꿔가면서 설정)
         (rects_temp, _) = self.pedes_detector.detectMultiScale(img_gray, winStride=(4, 4), padding=(2, 2), scale=32)
-        
         if len(rects_temp) != 0:
             
             #TODO: (6)  NMS 후처리 통과
             rects = non_maximum_supression(rects_temp)
 
             for (x,y,w,h) in rects:
-
                 #TODO: (8)  추출된 좌표로 박스 생성
                 cv2.rectangle(img_bgr, (x,y),(x+w,y+h),(0,255,255), 2)
 
@@ -116,9 +103,6 @@ class PEDESDetector:
 
 
 if __name__ == '__main__':
-
     rospy.init_node('pedes_detector', anonymous=True)
-
     pedes_detector = PEDESDetector()
-
     rospy.spin() 
