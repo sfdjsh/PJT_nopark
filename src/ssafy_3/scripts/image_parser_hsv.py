e6b18d43eb204b4670a9d0f6acba2eb9ef078cde
여동준 <tgc03042@naver.com>
2022-09-26 13:50:22 +0900

    Sub3_5 ~ 3_10 finish

--- src/ssafy_3/scripts/image_parser_hsv.py
+++ src/ssafy_3/scripts/image_parser_hsv.py
@@ -24,47 +24,28 @@ class IMGParser:
 
     def callback(self, msg):
         try:
-            '''
-
-            np_arr = np.fromstring(             )
-            img_bgr = cv2.imdecode(             )
-
-            '''
+            np_arr = np.fromstring(msg.data, dtype=np.uint8)
+            img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         except CvBridgeError as e:
             print(e)
 
         #TODO: (1)
-        '''
         # cv2.cvtColor 함수를 이용하여 bgr 이미지를 hsv 이미지로 변환합니다.
         # cv2.cvtColor 함수는 색상 공간 변환 함수로 이미지를 원하는 색상 코드로 변환합니다.
         # 이번 예제에서는 bgr이미지를 HSV 이미지로 변환합니다.
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
 
-        img_hsv = cv2.cvtColor(                 )
-
-        '''
         #TODO: (2)
-        '''
         # np.concatenate 함수는 는 서로 다른 두 배열을 합치는 예제입니다.
         # bgr 이미지와 hsv 이미지를 동시에 출력하기 위해 np.concatenate 함수를 사용해 이미지를 합칩니다.
-
-        img_concat = np.concatenate(            )
-
-        '''
+        img_concat = np.concatenate([img_bgr, img_hsv], axis=1)
 
         #TODO: (3)
-        '''
         # 이미지를 출력 합니다.
-
-        cv2.imshow(         )
-        cv2.waitKey(        ) 
-
-        '''
-
+        cv2.imshow('hsv', img_concat)
+        cv2.waitKey(1)
 
 if __name__ == '__main__':
-
     rospy.init_node('image_parser', anonymous=True)
-
     image_parser = IMGParser()
-
     rospy.spin() 
